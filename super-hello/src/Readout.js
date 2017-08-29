bdb7a67fc2505779b6d32fbbcb6acc9d77178b5e
Mac User <sabbey37@gmail.com>
2017-08-29 15:12:51 -0400

    adds react app

---
+++ super-hello/src/Readout.js
@@ -0,0 +1,8 @@
+const formatUnits = units => units.toUpperCase(); 
+const Readout = ({temperature, unit}) => (
+    <div>
+        <span>{temperature} {formatUnits(unit)}</span>
+    </div>
+);
+
+export default Readout;
